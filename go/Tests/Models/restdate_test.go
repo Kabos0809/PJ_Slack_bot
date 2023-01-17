20d0861a437866813209aabe0f246ff03f798c0b
Kabos0809 <hiyamazu70@gmail.com>
2023-01-17 14:12:34 +0900

    RestDate国、数、英以外の教科を入力した際のエラーテスト追加

--- go/Tests/Models/restdate_test.go
+++ go/Tests/Models/restdate_test.go
@@ -129,3 +129,70 @@ func TestDeleteRestDate(t *testing.T) {
 		t.Fatalf("[FATAL] Failed to Delete RestDate: %s", err)
 	}
 }
+
+func TestCreateFatal(t *testing.T) {
+	godotenv.Load("../../.env")
+	
+	dsn := Config.DbUrl()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+	
+	err = db.AutoMigrate(&Models.RestDate{}, &Models.Student{}, &Models.School{})
+	if err != nil {
+		panic(err)
+	}
+
+	m := Models.Model{Db: db}
+	
+	testSchool := Models.School{
+		ID: uuid.New(),
+		Name: "TestSchool1",
+	}
+	if err := m.AddSchool(&testSchool); err != nil {
+		t.Fatalf("[FATAL] Failed to AddSchool: %s", err)
+	}
+
+	school, err := m.TestGetFirstSchool(testSchool.ID)
+	if err != nil {
+		t.Fatalf("[FATAL] Failed to Get School: %s", err)
+	}
+
+	testStudent := Models.Student{
+		ID: uuid.New(),
+		FirstName: "hoge",
+		LastName: "henyo",
+		Name: "hogehenyo",
+		Grade: "テスト",
+		SchoolID: school.ID,
+	}
+	if err := m.CreateStudent(&testStudent); err != nil {
+		t.Fatalf("[FATAL] Failed to CreateStudent: %s", err)
+	}
+
+	if err := m.AddStudent4School(&testStudent, testStudent.SchoolID); err != nil {
+		t.Fatalf("[FATAL] Failed to Add Student for School: %s", err)
+	}
+
+	date, _ := time.Parse(testdate, format)
+
+	testRestDate := Models.RestDate{
+		ID: uuid.New(),
+		StudentID: testStudent.ID,
+		Date: date,
+		Subject: "hogehoge",
+	}
+
+	if err := m.CreateRestDate(&testRestDate); err != nil {
+		t.Fatalf("[FATAL] Failed to CreateRestDate: %s", err)
+	}
+
+	if err := m.AddRestDate4Student(&testRestDate, testRestDate.StudentID); err != nil {
+		t.Fatalf("[FATAL] Failed to Add RestDate for Student: %s", err)
+	}
+}
--- go/Tests/Models/school_test.go
+++ go/Tests/Models/school_test.go
@@ -1,12 +1,1 @@
 package test
-
-import (
-	"testing"
-
-	"gorm.io/gorm"
-	"gorm.io/driver/postgres"
-	"github.com/joho/godotenv"
-	"github.com/google/uuid"
-	"github.com/kabos0809/slack_bot/go/Models"
-	"github.com/kabos0809/slack_bot/go/Config"
-)
