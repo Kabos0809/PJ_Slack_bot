b1c65ce998c0356eb55e8674090afb1070df3d32
Kabos0809 <hiyamazu70@gmail.com>
2023-02-10 00:59:38 +0900

    Config.go ModelInterface削除

--- go/Models/Config.go
+++ go/Models/Config.go
@@ -5,25 +5,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type ModelInterface interface {
-	GetRestDatebyID(id uuid.UUID) (*RestDate, error)
-	GetRestDateFromStudent(studentID uuid.UUID) (*[]RestDate, error)
-	CreateRestDate(rdate *RestDate) error
-	DeleteRestDate(id uuid.UUID) error
-	GetAllSchool() (*[]School, error)
-	GetSchoolbyID(id uuid.UUID) (*School, error)
-	AddSchool(school *School) error
-	DeleteSchool(id uuid.UUID) error
-	TestGetFirstSchool(id uuid.UUID) (*School, error)
-	GetStudentbySchoolAndGrade(school string, grade string) (*[]Student, error)
-	GetStudentbyID(id uuid.UUID) (*Student, error)
-	CreateStudent(student *Student) error
-	DeleteStudent(id uuid.UUID) error
-	AddRestDate4Student(rdate *RestDate, id uuid.UUID) error
-	DeleteRestFromStudent(rdate *RestDate, id uuid.UUID) error
-	TransferCount(id uuid.UUID) (TransferCounts, error)
-}
-
 type Model struct {
 	Db *gorm.DB
 }
